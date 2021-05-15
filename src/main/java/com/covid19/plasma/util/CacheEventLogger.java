2cdc6c241aae191f59bc6eae5458cedbdda6d243
Vivek Pratap Singh <vivekpratap_singh@intuit.com>
2021-05-15 18:37:15 +0530

    Added in-memory cache support using ehcache

--- src/main/java/com/covid19/plasma/util/CacheEventLogger.java
+++ src/main/java/com/covid19/plasma/util/CacheEventLogger.java
@@ -1,4 +1,17 @@
 package com.covid19.plasma.util;
 
-public class CacheEventLogger {
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
+
+    Logger logger = LoggerFactory.getLogger(CacheEventLogger.class);
+    
+    @Override
+    public void onEvent(CacheEvent<?, ?> cacheEvent) {
+        logger.info("Cache event "+ cacheEvent.getType() +" for item with key " + cacheEvent.getKey() + ". Old value = " + cacheEvent.getOldValue() 
+                + " ,New value = " + cacheEvent.getNewValue());
+    }
 }
